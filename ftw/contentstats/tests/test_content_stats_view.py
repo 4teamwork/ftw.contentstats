06484ce6162891460228c375630feae5e61bd16a
Lukas Graf <lukas.graf@4teamwork.ch>
2016-12-22 16:48:55 +0100

    Disable editable border and byline for @@content-stats view.

--- ftw/contentstats/tests/test_content_stats_view.py
+++ ftw/contentstats/tests/test_content_stats_view.py
@@ -38,3 +38,8 @@ class TestContentStatsView(FunctionalTestCase):
         browser.login().open(self.portal, view='@@content-stats')
         table = browser.css('#content-stats-type-counts').first
         self.assertEqual([['Document', '2'], ['Folder', '1']], table.lists())
+
+    @browsing
+    def test_editable_border_disabled(self, browser):
+        browser.login().open(self.portal, view='@@content-stats')
+        self.assertEqual(0, len(browser.css('#content-views')))
