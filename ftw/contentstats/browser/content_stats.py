1be745491fae5bd2e57669f3bd5938326f1154fc
Lukas Graf <lukas.graf@4teamwork.ch>
2016-12-23 03:41:55 +0100

    Move JS to template.

--- ftw/contentstats/browser/content_stats.py
+++ ftw/contentstats/browser/content_stats.py
@@ -8,13 +8,6 @@ class ContentStats(BrowserView):
     """Displays content statistics.
     """
 
-    def get_pie_chart(self):
-        prefix = 'pie'
-        data = self.get_type_counts()
-        titles = self.get_type_titles()
-        chart = ChartBuilder(data, titles, prefix)
-        return chart.render()
-
     def get_type_titles(self):
         portal_types = api.portal.get_tool('portal_types')
         ftis = portal_types.values()
@@ -23,6 +16,9 @@ class ContentStats(BrowserView):
                 fti.title, domain=fti.i18n_domain, context=self.request))
             for fti in ftis]
         return dict(titles)
+
+    def get_type_titles_json(self):
+        return json.dumps(self.get_type_titles())
 
     def get_type_counts(self):
         """Return a list of (portal_type, count) tuples.
@@ -39,68 +35,5 @@ class ContentStats(BrowserView):
 
         return sorted(counts.items())
 
-
-class ChartBuilder(object):
-    """Build the JavaScript required to render a C3 Chart.
-    """
-
-    JS = """\
-    var chart = c3.generate({
-        bindto: '#%(chart_id)s',
-        data: {
-            columns: %(columns)s,
-            names: %(names)s,
-            type : 'pie',
-            legend: false
-        },
-        size: {
-            height: 360,
-            width: 480
-        },
-        pie: {
-            label: {
-                format: function (value, ratio, id) {
-                    return value;
-                }
-            }
-        }
-    });
-
-    $("#content-stats-type-counts tr")
-        .on("mouseover", (event) => {
-            chart.focus(event.currentTarget.dataset.id)
-    });
-
-    $("#content-stats-type-counts tr")
-        .on("mouseout", (event) => {
-            chart.focus();
-    });
-
-    $("#content-stats-type-counts tr")
-        .each(function( index ) {
-            var data_id = this.dataset.id;
-            d3.select(this).selectAll('td .legend-color')
-                .style('background-color', chart.color(data_id)
-    )});
-
-    chart.legend.hide();
-    """
-
-    MARKUP = """\
-    <div id="%(chart_id)s"></div>
-    <script type="text/javascript">%(js)s</script>
-    """
-
-    def __init__(self, data, titles, prefix):
-        self.data = data
-        self.titles = titles
-        self.prefix = prefix
-        self.chart_id = '%s-chart' % prefix
-
-    def render(self):
-        data_columns = json.dumps(self.data)
-        data_names = json.dumps(self.titles)
-        js = self.JS % dict(
-            columns=data_columns, names=data_names, chart_id=self.chart_id)
-        markup = self.MARKUP % dict(chart_id=self.chart_id, js=js)
-        return markup
+    def get_type_counts_json(self):
+        return json.dumps(self.get_type_counts())
